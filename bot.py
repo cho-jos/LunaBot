a97db3972afe411e2321d1fdf6f9a72150342073
Joseph Cho <jcho@Josephs-MacBook-Pro.local>
2024-08-14 11:58:03 -0400

    small refactor & add meow slash command

--- bot.py
+++ bot.py
@@ -1,31 +1,33 @@
-# following along https://realpython.com/how-to-make-a-discord-bot-python/ with some help from comment section
 import os
-
 import discord
+from discord import app_commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-#intents = discord.Intents(2048)
 intents = discord.Intents.default()
 intents.messages = True
 
-# get client object from discord.py. Client is synonymouse with bot
-client = discord.Client(intents=intents)
+# get client object from discord.py. Client is synonymous with bot
+bot = discord.Client(command_prefix='!', intents=intents)
+tree = app_commands.CommandTree(bot) # tree holds all the application commands
 
 # listener for when the bot has switched from offline to online
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     if message.content == 'hello!':
         response = f'Hello, {message.author.name}'
-        #response = "meow!"
         await message.channel.send(response)
 
-client.run(TOKEN)
+@tree.command(name='meow')
+async def meow(interaction: discord.Interaction):
+    await interaction.response.send_message("meow!")
+
+bot.run(TOKEN)
