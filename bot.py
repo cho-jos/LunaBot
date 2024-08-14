4c8adad08940d16a56d3503eb992c96fc8bb195a
Joseph Cho <jcho@Josephs-MacBook-Pro.local>
2024-08-14 15:32:20 -0400

    switch to pycord

--- bot.py
+++ bot.py
@@ -1,35 +1,22 @@
-import os
 import discord
-from discord import app_commands
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+bot = discord.Bot()
 
-intents = discord.Intents.default()
-intents.messages = True
-
-# get client object from discord.py. Client is synonymous with bot
-bot = discord.Client(command_prefix='!', intents=intents)
-tree = app_commands.CommandTree(bot) # tree holds all the application commands
-
-# listener for when the bot has switched from offline to online
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+    print(f'{bot.user} is connected to Discord!')
 
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
-        return
-    if message.content == 'hello!':
-        response = f'Hello, {message.author.name}'
-        await message.channel.send(response)
+    if message.content.lower() == 'hello!':
+        await message.channel.send(f'Hello, {message.author.name}')
 
-# TODO: extract slash commands to another file?
-# slash commands
-@tree.command(name='meow')
-async def meow(interaction: discord.Interaction):
-    await interaction.response.send_message("meow!")
+# TODO: move slash commands to external files (cogs?)
+@bot.slash_command(name='meow')
+async def meow(ctx: discord.ApplicationContext):
+    await ctx.respond('meow!')
 
-bot.run(TOKEN)
+bot.run(os.getenv('DISCORD_TOKEN'))
