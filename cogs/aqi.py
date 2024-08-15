7dffa131d9f992b2fdae97023e2518d8b1551edf
Joseph Cho <cho.joseph17@gmail.com>
2024-08-15 17:38:20 -0400

    add aqi_call command

--- cogs/aqi.py
+++ cogs/aqi.py
@@ -26,7 +26,7 @@ class AqiCog(commands.Cog):
         elif aqi > 300:
             return ':rotating_light: HARZARDOUS!'
 
-    # TODO: then get the aqi for other places, also make this more programmatic
+    # TODO: searching by city name seems highly unreliable, need to figure out consistent search
     @discord.command()
     async def aqi(self, ctx, arg):
         r = requests.get(self.AQICN_BASE_URL + f"{arg}/?token=" + self.AQICN_TOKEN)
@@ -40,6 +40,38 @@ class AqiCog(commands.Cog):
         else:
             print(f'Error calling AQICN API: {r.status_code}')
             await ctx.respond('Error calling AQICN API')
+
+    # get the aqi for some specific areas
+    # TODO: consolidate reports into one string. Right now the bot times out before the response is ready
+    @discord.command()
+    async def aqi_all(self, ctx):
+        places = {
+            '@5883': 'Boston',
+            'A422149': 'Pearl City',
+            'A406096': 'Philadelphia',
+            'A366688': 'Santa Ana',
+            '@5508': 'Seoul',
+            'A359320': 'Somerville',
+            'A378043': 'Wayne',
+            'A475411': 'Yonkers'
+        }
+        aqi_report = ''
+        for station in places.keys():
+            r = requests.get(self.AQICN_BASE_URL + f'{station}/?token=' + self.AQICN_TOKEN)
+            if r.status_code == 200:
+                location = places[station]
+                r_json = r.json()
+                if r_json['status'] != 'ok':
+                    await ctx.respond(f"Cannot retrieve AQI near \"{location}\". \n Error: {r_json['data']}")
+                    #aqi_report += f"Cannot retrieve AQI near \"{location}\". \n Error: {r_json['data']}\n"
+                else:
+                    aqi = r_json['data']['aqi']
+                    await ctx.respond(f"The aqi near {location}: {aqi} {self.add_aqi_emojis(aqi)}")
+                    #aqi_report += f"The aqi near {location}: {aqi} {self.add_aqi_emojis(aqi)}\n"
+            else:
+                print(f'Error calling AQICN API: {r.status_code}')
+                await ctx.respond('Error calling AQICN API')
+        #await ctx.respond(aqi_report)
     
 def setup(bot):
     bot.add_cog(AqiCog(bot))
